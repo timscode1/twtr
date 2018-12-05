5ca714d0e685c401b4b76f21fdc36fca29e08ed0
tim <timscode1@gmail.com>
2018-12-05 09:21:32 -0500

    origin

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  serialize :following, Array
+
+  has_many :tweets
+
+end
